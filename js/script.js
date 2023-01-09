be9f2d9697c3b95a4591c21ee06708a219102968
antonionovelli92 <anto.novelli@hotmail.it>
2023-01-09 17:17:23 +0100

    todo: error

--- js/script.js
+++ js/script.js
@@ -25,7 +25,7 @@ Buon lavoro e buon divertimento! : faccia_leggermente_sorridente:
 PS: avviso: Vi ricordo anche qui che l'orario dei ticket oggi è ridotto: 15:00 - 17:00
 */
 
-const data = [
+const pictrues = [
     {
         image: 'img/01.webp',
         title: 'Marvel\'s Spiderman Miles Morale',
@@ -48,3 +48,111 @@ const data = [
         text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
     }
 ];
+
+// ! FUNZIONI.......
+function changePic(target) {
+
+    // Rimuovo la classe active dall'immagine corrente
+    galleryFigures[currentActiveIndex].classList.remove('active');
+    thumbnailImages[currentActiveIndex].classList.remove('active');
+
+    if (target === 'next') {
+        // incremento l'inidce
+        currentActiveIndex++;
+
+        // ? controlliamo se siamo oltre l'array
+        if (currentActiveIndex === galleryFigures.length) currentActiveIndex = 0;
+
+    } else if (target === 'prev') {
+        // decrementro l'indice
+        currentActiveIndex--;
+
+        // ? controlliamo se siamo in negativo
+        if (currentActiveIndex < 0) currentActiveIndex = galleryFigures.length - 1;
+    } else {
+        currentActiveIndex = target;
+    }
+    // assegno la classe active col nuovo indice
+    galleryFigures[currentActiveIndex].classList.add('active');
+    thumbnailImages[currentActiveIndex].classList.add('active');
+}
+
+
+// ! OPERAZIONI INIZIALI.......
+// Recupero i bottoni
+const prev = document.getElementById('prev');
+const next = document.getElementById('next');
+
+// Recupero la Gallery
+const gallery = document.querySelector('#carousel .gallery');
+
+// Recupero anche la galleria dei thumbnails
+const thumbGallery = document.getElementById('thumbnails');
+
+let galleryElements = '';
+let thumbnailElements = '';
+
+
+pictrues.forEach((pictrue, i) => {
+    const img = `<img src="${pictrue.image}" alt="games_${i}">`;
+    thumbnailElements += img;
+
+    galleryElements += `
+        <figure>
+           ${img}
+           <figcaption>
+                <h2>${pictrue.title}</h2>
+                <h3>${pictrue.text}</h3>
+           </figcaption>
+        </figure>
+     `;
+});
+
+gallery.innerHTML = galleryElements;
+thumbGallery.innerHTML = thumbnailElements;
+
+
+// Recupero le immagini
+const galleryFigures = document.querySelectorAll('.gallery figure');
+// Recupero i thumbnails
+const thumbnailImages = document.querySelectorAll('#thumbnails img');
+
+// Metto la prima immagine come active
+let currentActiveIndex = 0;
+galleryFigures[currentActiveIndex].classList.add('active');
+
+// anche ai thumbnails
+thumbnailImages[currentActiveIndex].classList.add('active');
+
+
+
+
+// ! EVENTI DINAMICI.......
+// aggancio evento al bottone next
+next.addEventListener('click', function () {
+    changePic('next');
+});
+
+// aggancio evento al bottone prev
+prev.addEventListener('click', function () {
+    changePic('prev');
+});
+
+// Rendiamo cliccabili i thumbnails
+thumbnailImages.forEach((thumb, i) => {
+    thumb.addEventListener('click', function () {
+        changePic(i);
+    });
+});
+
+
+
+// for (let i = 0; i < thumbnailsImages.length; i++) {
+//     // recupero di volta in volta un thumb...
+//     const thumb = thumbnailsImages[i];
+
+//     thumb.addEventListener('click', function () {
+//         changePic(i);
+//     });
+// }
+
